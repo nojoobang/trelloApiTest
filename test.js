a8997c7075b462cd29de1688250f0166959826ea
Jeawon <kakaroto12@naver.com>
2013-09-25 00:25:01 +0900

    ...

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-var Trello = require("node-trello");
-var t = new Trello("bc2746ad21a253b505fa27438f1fa256", "588f337cb358e08704d6e0bce503021959d7cda897decc5c6f4e777433e4ddf7");
+// var Trello = require("node-trello");
+// var t = new Trello("bc2746ad21a253b505fa27438f1fa256", "588f337cb358e08704d6e0bce503021959d7cda897decc5c6f4e777433e4ddf7");
 
 // t.get("/1/boards/5215db861bbe57d35f001a2e/cards?fields=idBoard,idList,idMembers,name,url", function(err, data) {
 //   if (err) throw err;
@@ -20,7 +20,19 @@ var t = new Trello("bc2746ad21a253b505fa27438f1fa256", "588f337cb358e08704d6e0bc
 //   console.log(data);
 // });
 
-t.get("/1/cards/523c4fe243b4a4ef2c003a5b/", function(err, data) {
-  if (err) throw err;
-  console.log(data);
+// t.get("/1/cards/523c4fe243b4a4ef2c003a5b/", function(err, data) {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+
+var express = require('express'),
+	app = express();
+
+app.get('/', function(req, res) {
+	res.send('Hello World');
+});
+
+app.listen(3303, function() {
+	console.log('Server Start');
 });
